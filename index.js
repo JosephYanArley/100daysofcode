1b094a22e68dc9d330d8a129a16471e6c0fb5df2
Антон Поздняков <breezae@gmail.com>
2017-05-06 14:08:10 +0300

    20 день, панель управления

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-//var express = require('express');
-//var app = express();
-//var http = require('http').Server(app);
-//var io = reqiure('socket.io')(http);
-//app.set('port', (process.env.PORT || 5000));
-
-//app.get('/', function(req, res){
-//res.sendFile(__dirname + '/public_html/index.html');
-//});
-
-//io.on('connection', function(socket){
-//  socket.on('chat message', function(msg){
-//    io.emit('chat message', msg);
-//  });
-//});
-
-//app.listen(app.get('port'), function() {
-//  console.log('Node app is running on port', app.get('port'));
-//});
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var express = require('express');
+
+app.use(express.static(__dirname + '/public_html/'));
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/public_html/index.html');
+res.sendFile(__dirname + '/public_html/');
 });
 
 io.on('connection', function(socket){
