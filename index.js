3f7d9c6fd9922ada81c10a87c969e7a2055b1882
Антон Поздняков <breezae@gmail.com>
2017-05-07 23:21:10 +0300

    День 21

--- index.js
+++ index.js
@@ -10,10 +10,44 @@ app.get('/', function(req, res){
 res.sendFile(__dirname + '/public_html/');
 });
 
+var array_sprite = {};
+
 io.on('connection', function(socket){
   socket.on('socket_pack', function(msg){
     io.emit('socket_pack', msg);
   });
+	socket.emit('sprites_21', array_sprite);				//отправляем массив текущему пользователю
+    socket.broadcast.emit('sprites_21', array_sprite);		//отправляем массив всем пользователям. Загрузка для вновь подключившихся
+    socket.on('sprites_21', function (data) {
+        if (socket.id in array_sprite) {					//если свойство socket.id элемента массива есть
+            switch (data.command) {
+                case 'up':									//отрабатываем движение
+                    array_sprite[socket.id].y -= 10;
+                    break;
+                case 'down':
+                    array_sprite[socket.id].y += 10;
+                    break;
+                case 'right':
+                    array_sprite[socket.id].x += 10;
+                    break;
+                case 'left':
+                    array_sprite[socket.id].x -= 10;
+                    break;
+            }
+        } else {
+            array_sprite[socket.id] = {							//иначе создаем элемент с начальными данными
+                x: 225,
+                y: 225
+            };
+        }
+
+        socket.broadcast.emit('sprites_21', array_sprite);
+    });
+
+    socket.on('disconnect', function(){
+        delete array_sprite[socket.id];
+        socket.broadcast.emit('sprites_21', array_sprite);
+    });
 });
 
 http.listen(port, function(){
